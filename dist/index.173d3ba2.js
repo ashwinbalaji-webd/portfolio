04917d1d6ec52ff12d7c851bfa66c00ab23bc0ca
Ashwin Balaji S <ashwinbalaji@infinitisoftware.net>
2024-03-23 12:53:43 +0530

    Add build files

--- dist/index.173d3ba2.js
+++
@@ -1,2 +0,0 @@
-const s=[...document.querySelectorAll(".cls-aos-X, .cls-aos-Y")],t=new IntersectionObserver(function(s,t){for(let e of s)e.isIntersecting?(e.target.classList.add(e.target.classList.contains("cls-aos-X")?"cls-translateX":"cls-translateY"),e.target.classList.contains("cls-aos-Y")&&t.unobserve(e.target)):e.target.classList.remove("cls-translateX","cls-translateY")},{root:null,rootMargin:"0px",threshold:.3});s.forEach(s=>t.observe(s));
-//# sourceMappingURL=index.173d3ba2.js.map
