7fed84c296ecdc97cf1b7045482771c5ed76018b
Ashwin Balaji S <ashwinbalaji@infinitisoftware.net>
2024-03-26 18:58:58 +0530

    Add blogs section

--- src/js/main.js
+++ src/js/main.js
@@ -5,14 +5,14 @@ const elements = [...document.querySelectorAll(".cls-aos-X, .cls-aos-Y")];
 let options = {
   root: null,
   rootMargin: "0px",
-  threshold: 0.1, // [1, 0.2, 0.5] // 50% intersection
+  threshold: 0.15, // [1, 0.2, 0.5] // 50% intersection
 };
 
-const observer = new IntersectionObserver(callback, options);
+const observer = new IntersectionObserver(YAnimationCallback, options);
 
 elements.forEach((element) => observer.observe(element));
 
-function callback(entries, observer) {
+function YAnimationCallback(entries, observer) {
   for (const entry of entries) {
     if (entry.isIntersecting) {
       entry.target.classList.add(
@@ -47,7 +47,7 @@ const roles = [
   "React Developer",
   "Django Developer",
   "API Developer",
-  "Web Developer"
+  "Web Developer",
 ];
 
 let currentIndex = 0;
@@ -59,3 +59,28 @@ setInterval(() => {
 
   if (currentIndex === roles.length) currentIndex = 0;
 }, 1000);
+
+/* Progress animation */
+const progressContainer = document.getElementById("progress");
+
+const progressObserver = new IntersectionObserver(
+  progressAnimationCallback,
+  options
+);
+
+progressObserver.observe(progressContainer);
+
+function progressAnimationCallback(entries, observer) {
+  for (const entry of entries) {
+    if (entry.isIntersecting) {
+      {
+        const progressEls = document.querySelectorAll(".cls-progress-bar");
+        progressEls.forEach((progressEl) => {
+          const progressPercentage = progressEl.getAttribute("data-progress");
+          progressEl.style.setProperty("width", `${progressPercentage}%`);
+        });
+      }
+      observer.unobserve(entry.target);
+    }
+  }
+}
